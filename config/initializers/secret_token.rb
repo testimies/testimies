ed71f3ad23694077db1aa0712c78a33e54d2e4e5
testimies <cm@casamedia.fi>
2011-09-27 07:30:18 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testimies::Application.config.secret_token = '6c77f61df80b429984964bb9016f915db905347dba96084bbea81cac42e4967fe1c0d7ca61c618c51e0f4e9bc90b2d580bac74de8b798179f016027a4724e8a9'
